763703e3a179e7810298010e3acc1985dc871d80
jeanpanamito <jppanamito@utpl.edu.ec>
2022-05-17 23:44:39 -0500

    estructuras

---
+++ myArray/src/myarray/MyArray.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myarray;
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+/**
+ *
+ * @author JEanpa
+ */
+public class MyArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      matrices01 obj = new matrices01(4, 4);
+        Scanner entrada = new Scanner(System.in);
+
+        int op;
+        int[][] A = new int[3][4];
+        int[][] B = new int[4][2];
+        int [][]C= new int [3][2];
+        do {
+            op = obj.menu();
+            switch (op) {
+                case 1:
+                    obj.leerMatriz();
+
+                    break;
+                case 2:
+                    obj.presentaMatriz();
+                    break;
+                case 3:
+                    obj.trasponer();
+                    break;
+                case 4:
+                    obj.transponerDiagonalSecundaria();
+                    break;
+                case 5:
+                    obj.leerMatriz(A);
+                    obj.leerMatriz(B);
+                    obj.multiplica(A,B,C);
+                    obj.presentaMatriz(C);
+                    break;
+                case 6:
+                    System.out.println("__________________");
+                    obj.leerMatriz(A);
+                    obj.presentaMatriz(A);
+                    System.out.println("--------------------");
+                     System.out.println(obj.determinante(A));
+                    System.out.println("____________________");
+                   
+                   
+                    break;
+                
+                    
+                default: break;
+
+            }
+        } while (op != 0);
+    }
+
+}
+
+
+
+    
+    
+
---
+++ myArray/src/myarray/matrices01.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myarray;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JEanpa
+ */
+public class matrices01 {
+Scanner entrada;
+    int[][] myMatriz;
+
+   
+    public matrices01(int fil, int col) {
+        this.entrada = new Scanner(System.in);
+        this.myMatriz = new int[fil][col];
+
+    }
+
+    public int menu() {
+        int op;
+        System.out.println("Leer Matriz                     [1]:");
+        System.out.println("Presentar Matriz                [2]:");
+        System.out.println("Transponer                      [3]:");
+        System.out.println("Transponer matriz Secundario:   [4]");
+        System.out.println("Multiplicacion de matrices      [5]:");
+        System.out.println("Determinante                    [6]:");
+        System.out.println("Salir                           [0]:");
+
+        op = entrada.nextInt();
+        return op;
+    }
+
+    public void leerMatriz() {
+        for (int i = 0; i < myMatriz.length; i++) {
+            for (int j = 0; j < myMatriz.length; j++) {
+                System.out.println("Ingrese elemento [" + i + "][" + j + "]:");
+                myMatriz[i][j] = entrada.nextInt();
+
+            }
+
+        }
+    }
+
+    public void leerMatriz(int[][] x) {
+        int l = 0;
+        System.out.println("Matriz "+(l+1));
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[0].length; j++) {
+                System.out.println("Ingrese elemento [" + i + "][" + j + "]:");
+                x[i][j] = entrada.nextInt();
+
+            }
+            
+
+        }
+    }
+
+    public void multiplica(int[][] a, int[][] b, int[][] c) {
+        int S;
+        for (int i = 0; i < a.length; i++) {
+            for (int l = 0; l < b[0].length; l++) {
+                S = 0;
+                for (int j = 0; j < a[0].length; j++) {
+                    S += a[i][j] * b[j][l];
+                }
+                c[i][l] = S;
+            }
+        }
+    }
+
+    public void presentaMatriz(int[][] x) {
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[0].length; j++) {
+                System.out.print(x[i][j] + " ");
+
+            }
+            System.out.println(" ");
+
+        }
+    }
+
+    public void presentaMatriz() {
+        for (int i = 0; i < myMatriz.length; i++) {
+            for (int j = 0; j < myMatriz.length; j++) {
+                System.out.print(myMatriz[i][j] + " ");
+
+            }
+            System.out.println(" ");
+
+        }
+    }
+
+    public void trasponer() {
+        int aux;
+        for (int i = 0; i < myMatriz.length - 1; i++) {
+            for (int j = i + 1; j < myMatriz[0].length; j++) {
+                aux = myMatriz[i][j];
+                myMatriz[i][j] = myMatriz[j][i];
+                myMatriz[j][i] = aux;
+
+            }
+
+        }
+    }
+
+    public void transponerDiagonalSecundaria() {
+        int aux;
+        int l;
+        int k;
+        k = myMatriz.length - 1;
+        for (int i = 0; i < myMatriz.length - 1; i++) {
+            l = myMatriz.length - 1;
+            for (int j = 0; j < (myMatriz.length - 1 - i); j++) {
+                aux = myMatriz[i][j];
+                myMatriz[i][j] = myMatriz[l][k];
+                myMatriz[l][k] = aux;
+
+                l--;
+            }
+            k--;
+
+        }
+
+    }
+
+   public int determinante(int[][] X) {
+        int Sp = 0, Sn = 0, Ap, An, j, l;
+        for (var k = 0; k < X.length; k++) {
+            j = X.length - 1 - k;
+            l = X.length - 1 - k;
+            Ap = 1;
+            An = 1;
+            for (var X1 : X) {
+                Ap *= X1[j];
+                An *= X1[l];
+                if (j == X.length - 1) {
+                    j = 0;
+                } else {
+                    j++;
+                }
+                if (l == 0) {
+                    l = X.length - 1;
+                } else {
+                    l--;
+                }
+            }
+            Sp += Ap;
+            Sn += An;
+        }
+        return Sp - Sn;
+    }
+}
+
+    
